88ca23c33ee91c2f6702ef1a0ff63fca8538bb22
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-23 17:00:33 +0800

    Finished

--- tax_pay.py
+++ tax_pay.py
@@ -1,15 +1,18 @@
 # Calculate Income Tax
 
 # Given
-money_income = 23000
+money_income = 700000
 payable_tax = 0
 print("your income:", money_income)
 
 # Calculate 
 if money_income <= 10000:
     payable_tax = 0
-print (payable_tax)
-
-
+elif money_income <= 20000:
+    payable_tax = (money_income - 10000)* 10 / 100
+else:
+    payable_tax = 10000 * 10 / 100
+    payable_tax += (money_income - 20000)* 20 / 100
 
 # Print
+print("Your total tax to pay is:", payable_tax)
